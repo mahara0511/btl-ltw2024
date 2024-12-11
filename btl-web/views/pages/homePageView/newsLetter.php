82c10a7a37e6d454b3d0f5b0ee3fdd73b8f93b70
Nguyen Truyen Tuan <tuan160404@gmail.com>
2024-12-11 17:02:02 +0700

    Fix footer

--- btl-web/views/pages/homePageView/newsLetter.php
+++ btl-web/views/pages/homePageView/newsLetter.php
@@ -4,12 +4,13 @@
         <!-- row -->
         <div class="row">
             <div class="col-md-12">
-            
+
                 <div class="newsletter">
                     <p>Sign Up for the <strong>OFFERUPDATES</strong></p>
                     <form id="offer_form" onsubmit="return false">
                         <input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
-                        <button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+                        <button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i
+                                class="fa fa-envelope"></i> Subscribe</button>
                     </form>
                     <div class="" id="offer_msg">
                         <!--Alert from signup form-->
@@ -22,10 +23,10 @@
                             <a href=""><i class="fa-brands fa-twitter"></i></a>
                         </li>
                         <li>
-                            <a href=""><i class="fa-brands fa-instagram"></i>  </a>
+                            <a href=""><i class="fa-brands fa-instagram"></i> </a>
                         </li>
                         <li>
-                            <a href=""><i class="fa-brands fa-github"></i>  </a>
+                            <a href=""><i class="fa-brands fa-github"></i> </a>
                         </li>
                     </ul>
                 </div>
